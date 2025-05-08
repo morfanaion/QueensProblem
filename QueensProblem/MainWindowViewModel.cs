dcf5988f596e9e06c049d0151b53bf1a6a0facf0
André Noordzij <noordzij.am@gmail.com>
2025-05-08 08:14:05 +0200

    AllSolutions

--- QueensProblem/MainWindowViewModel.cs
+++ QueensProblem/MainWindowViewModel.cs
@@ -1,21 +1,54 @@
 ﻿
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+
 namespace QueensProblem
 {
-	public class MainWindowViewModel
+	public class MainWindowViewModel : INotifyPropertyChanged
 	{
-		List<int[]> Solutions { get; set; }
+		private List<QueensSolution> _solutions = new List<QueensSolution>();
+		public List<QueensSolution> Solutions 
+		{ get => _solutions;
+			set
+			{
+				_solutions = value;
+				RaisePropertyChanged();
+			}
+		}
+
+		private QueensSolution _selectedSolution = new QueensSolution();
+		public QueensSolution SelectedSolution
+		{
+			get => _selectedSolution; 
+			set
+			{
+				_selectedSolution = value;
+				RaisePropertyChanged();
+			}
+		}
+
 		public MainWindowViewModel()
 		{
-			Solutions = new List<int[]>();
+			Solutions = new List<QueensSolution>();
+			Solve();
+			
+		}
+
+		public event PropertyChangedEventHandler? PropertyChanged;
+		private void RaisePropertyChanged([CallerMemberName]string? propertyName = null)
+		{
+			PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+		}
+
+		public void Solve()
+		{
 			QueensProblemSolver problem8 = new QueensProblemSolver(8);
 			problem8.Solve();
+			Solutions = problem8.Solutions.Zip(Enumerable.Range(1, problem8.Solutions.Count)).Select(pair => new QueensSolution() { Description = $"Solution {pair.Second}", QueensPositions = pair.First}).ToList();
 			CurrentQueenPositions = problem8.Solutions.First();
 		}
 
-		private void BuildQueenSolutions()
-		{
-			
-		}
+		
 
 		public int[] CurrentQueenPositions { get; set; } = new int[8];
 
---
+++ QueensProblem/QueensSolution.cs
@@ -0,0 +1,8 @@
+﻿namespace QueensProblem
+{
+	public class QueensSolution
+	{
+		public string Description { get; set; } = string.Empty;
+		public int[] QueensPositions { get; set; } = Array.Empty<int>();
+	}
+}
